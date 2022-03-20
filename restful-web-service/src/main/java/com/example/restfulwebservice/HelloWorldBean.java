834c5aa1389916632522bf8210fa68fe8b58e634
HONG-JUNG-WAN <hjw43ok@hs.ac.kr>
2022-03-20 20:18:22 +0900

    [test] bean test

---
+++ restful-web-service/src/main/java/com/example/restfulwebservice/HelloWorldBean.java
@@ -0,0 +1,15 @@
+package com.example.restfulwebservice;
+
+// lombok
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class HelloWorldBean {
+    private String message;
+
+}
---
+++ restful-web-service/src/main/java/com/example/restfulwebservice/HelloWorldController.java
@@ -0,0 +1,22 @@
+package com.example.restfulwebservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    // GET
+    // /hello-world (endpoint)
+    // @RequestMapping(method=RequestMethod.GET, path="/hello-world")   <- 옛날 방식
+
+    @GetMapping(path = "/hello-world")
+    public String helloWorld() {
+        return "Hello World";
+    }
+
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloWorldBean() {
+        return new HelloWorldBean("Hello World");
+    }
+
+}
