0a64a770130c6125d33f1c5dffc879a835e1d094
HONG-JUNG-WAN <hjw43ok@hs.ac.kr>
2022-03-21 23:24:02 +0900

    [test] test Bean

--- restful-web-service/src/main/java/com/example/restfulwebservice/HelloWorldController.java
+++ restful-web-service/src/main/java/com/example/restfulwebservice/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.example.restfulwebservice;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,6 +18,12 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world-bean")
     public HelloWorldBean helloWorldBean() {
         return new HelloWorldBean("Hello World");
+    }
+
+    // hello-world-bean/path-variable/kenneth
+    @GetMapping(path = "/hello-world-bean/path-variable/{name}")
+    public HelloWorldBean helloWorldBean2(@PathVariable String name) {
+        return new HelloWorldBean(String.format("Hello World, %s", name));
     }
 
 }
