43315fbde347d0fa071f65f0fda6f73dde99bd0c
HONG-JUNG-WAN <hjw43ok@hs.ac.kr>
2022-03-23 17:38:53 +0900

    [feat] GET HTTP Method [feat] POST HTTP Method [feat] HTTP Status Code

---
+++ restful-web-service/src/main/java/com/example/restfulwebservice/User/UserController.java
@@ -0,0 +1,42 @@
+package com.example.restfulwebservice.User;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+public class UserController {
+    private UserDaoService service;
+
+    public UserController(UserDaoService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/users")
+    public List<User> retrieveAllUsers() {
+        return service.findAll();
+    }
+
+    // GET /users/1 or /users/10 -> String
+    @GetMapping("/users/{id}")
+    public User retrieveUser(@PathVariable int id) {
+        return service.findOne(id);
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User saveUser = service.save(user);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(saveUser.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
+    }
+
+}
+
