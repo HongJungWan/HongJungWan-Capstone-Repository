424dad840897908a4c0c23ef24065756cc3b0ac0
HONG-JUNG-WAN <hjw43ok@hs.ac.kr>
2022-03-24 17:11:56 +0900

    [feat] AOP Exception Handling [feat] Delete HTTP

--- restful-web-service/src/main/java/com/example/restfulwebservice/User/UserDaoService.java
+++ restful-web-service/src/main/java/com/example/restfulwebservice/User/UserDaoService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
 
 @Service
@@ -39,6 +40,20 @@ public class UserDaoService {
             }
         }
 
+        return null;
+    }
+
+    public User deleteById(int id) {
+        Iterator<User> iterator = users.iterator();
+
+        while (iterator.hasNext()) {
+            User user = iterator.next();
+
+            if (user.getId() == id) {
+                iterator.remove();
+                return user;
+            }
+        }
         return null;
     }
 
