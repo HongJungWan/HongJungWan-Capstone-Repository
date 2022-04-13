0da250768e90e69adbe9b0ddf28386346269033e
HONG-JUNG-WAN <hjw43ok@hs.ac.kr>
2022-04-14 03:38:59 +0900

    [refactor] @XToOne LAZY

--- web/src/main/java/com/example/demo/domain/entity/Charge.java
+++ web/src/main/java/com/example/demo/domain/entity/Charge.java
@@ -24,7 +24,7 @@ public class Charge {
     @Column(name = "amount")
     private Integer amount;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "collage_id", nullable = false)
     private Collage collage;
 
