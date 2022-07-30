a26451f35e2200eb64b5f87d0a469ecf3f9a71e6
HongJungWan <hjw43ok@hs.ac.kr>
2022-07-30 14:26:54 +0900

    [feat] Notification UI

--- src/main/java/com/example/demo/controller/NotificationController.java
+++ src/main/java/com/example/demo/controller/NotificationController.java
@@ -9,11 +9,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
@@ -26,8 +25,8 @@ public class NotificationController {
     AndroidPushNotificationsService androidPushNotificationsService;
 
     //    @Scheduled(fixedRate = 10000) //10초 > 실제 서버 동작 시 1000*60*60*24 ( 24시간 )
-    @GetMapping(value = "/send")
-    public @ResponseBody ResponseEntity<String> send() throws JSONException, InterruptedException, IOException {
+    @PostMapping(value = "/send")
+    public @ResponseBody ResponseEntity<String> send() throws JSONException, InterruptedException {
         String notifications = AndroidPushPeriodicNotifications.PeriodicNotificationJson();
 
         HttpEntity<String> request = new HttpEntity<>(notifications);
