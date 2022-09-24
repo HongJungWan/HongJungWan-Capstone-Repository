21d453dd72731106bf6b9588deeada099ea41f88
HongJungWan <hjw43ok@hs.ac.kr>
2022-09-24 22:08:19 +0900

    test : UserTest

--- src/test/java/com/example/demo/domain/entity/UserTest.java
+++ src/test/java/com/example/demo/domain/entity/UserTest.java
@@ -3,12 +3,13 @@ package com.example.demo.domain.entity;
 import com.example.demo.domain.value.Gender;
 import com.example.demo.domain.value.Role;
 import com.example.demo.repository.UserRepository;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @Transactional
@@ -20,8 +21,9 @@ public class UserTest {
     @Test
     @DisplayName("유저 엔티티 테스트")
     void saveTest() {
+        // given
         User user = User.createUser()
-                .id("1")
+                .id("1000")
                 .car_number("12가 1212")
                 .name("홍")
                 .password("1234")
@@ -31,8 +33,20 @@ public class UserTest {
                 .email("test@naver.com")
                 .role(Role.ROLE_USER)
                 .build();
+
+        // when
         User savedUser = userRepository.save(user);
 
-        Assertions.assertThat(user.getEmail()).isEqualTo("test@naver.com");
+        // then
+        assertThat(savedUser.getEmail()).isEqualTo("test@naver.com");
+        assertThat(savedUser.getName()).isEqualTo("홍");
+        assertThat(savedUser.getCar_number()).isEqualTo("12가 1212");
+        assertThat(savedUser.getPassword()).isEqualTo("1234");
+        assertThat(savedUser.getAge()).isEqualTo(25);
+        assertThat(savedUser.getRole()).isEqualTo(Role.ROLE_USER);
+        assertThat(savedUser.getPhonNumber()).isEqualTo("010-0000-0000");
+        assertThat(savedUser.getGender()).isEqualTo(Gender.MALE);
+
     }
+
 }
