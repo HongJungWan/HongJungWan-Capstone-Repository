a7fdc4073c44d78d1fc7d87cf63b10b33c877464
HongJungWan <hjw43ok@hs.ac.kr>
2022-10-04 00:50:23 +0900

    test : UserRepositoryTest

---
+++ src/test/java/com/example/demo/repository/UserRepositoryTest.java
@@ -0,0 +1,29 @@
+package com.example.demo.repository;
+
+import com.example.demo.domain.entity.User;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@SpringBootTest
+@Transactional
+class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    @DisplayName("유저 특정 이메일 조회")
+    void findByEmail() {
+        Optional<User> list = userRepository.findByEmail("admin");
+
+        System.out.println("UserRepositoryTest.findByEmail = " + list.get().getEmail());
+
+        Assertions.assertThat(list.get().getEmail()).isEqualTo("admin");
+    }
+}
